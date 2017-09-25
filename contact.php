72fd14c5d19bbc126d1684974be06b2d313cd840
Tom <tom@tomnolane.ru>
2017-09-25 15:40:15 +0300

    fix spaces

--- contact.php
+++ contact.php
@@ -24,35 +24,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	
 	$res = json_decode($res); 
 
-if($res->success)
-{
-	if (isset($_POST['secondaymit'])) { 
-$to      = 'medvedev.alexandr88@yandex.ru';
-$subject = 'Письмо с сайта total-care';
-$message .= "Имя: ".$_POST["name"]."\r\n"; 
-$message .= "Email: ".$_POST["email"]."\r\n"; 
-$message .= "Контактный телефон:".$_POST['secondayject']."\r\n\r\n";  
-$message .= "Текст обращения:\r\n".nl2br($_POST["message"])."";
-$headers = 'From: info@total-care.ru' . "\r\n" . 
-    'X-Mailer: PHP/' . phpversion();
+	if($res->success)
+	{
+		if (isset($_POST['secondaymit'])) 
+		{ 
+			$to      = 'medvedev.alexandr88@yandex.ru';
+			$subject = 'Письмо с сайта total-care';
+			$message .= "Имя: ".$_POST["name"]."\r\n"; 
+			$message .= "Email: ".$_POST["email"]."\r\n"; 
+			$message .= "Контактный телефон:".$_POST['secondayject']."\r\n\r\n";  
+			$message .= "Текст обращения:\r\n".nl2br($_POST["message"])."";
+			$headers = 'From: info@total-care.ru' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
-
-
-if(mail($to, $subject, $message, $headers))
-{
-	$_POST['tt'] = 'Письмо было успешно отправлено !';
-	echo "<script>alert('Письмо было успешно отправлено !!');</script>";
-	echo '<meta http-equiv="refresh" content="0;URL=http://total-care.ru/">';
-	//header('Location: http://total-care.ru/ ');
-      
-}	
-else
-{
-	echo "<script>alert('Письмо не отправлено, поробуйте снова!');</script>";
-}
-
-	  }
-}	
+			if(mail($to, $subject, $message, $headers))
+			{
+				$_POST['tt'] = 'Письмо было успешно отправлено !';
+				echo "<script>alert('Письмо было успешно отправлено !!');</script>";
+				echo '<meta http-equiv="refresh" content="0;URL=http://total-care.ru/">';
+				//header('Location: http://total-care.ru/ '); 
+			}	
+			else
+			{
+				echo "<script>alert('Письмо не отправлено, поробуйте снова!');</script>";
+			} 
+	  	}
+	}	
 }
 
 
